69d7661b4a02508732de056b117a3a18dabba848
vibhuti2023 <129948528+vibhuti2023@users.noreply.github.com>
2025-02-28 15:40:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,6 @@ provider "aws" {
   region = var.region
 }
 
-provider "aws" {
-  region = "ap-southeast-2"  # Ensure this matches your S3 bucket's region
-}
-
-
 resource "aws_s3_bucket" "dr_backup" {
   bucket = var.s3_bucket_name
 }
@@ -37,6 +32,26 @@ resource "aws_instance" "dr_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+}
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_logs" {
+  name       = "lambda-logs"
+  roles      = [aws_iam_role.lambda_exec.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "dr_lambda" {
